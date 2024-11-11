e4a629c736e84c3e8ad31ccdfec920e0f8eceb47
jarw1th <parastaev31@gmail.com>
2024-11-11 20:03:35 +0300

    Initial

---
+++ ARFurniture/ARFurniture/ARFurnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFurnitureApp.swift
+//  ARFurniture
+//
+//  Created by Руслан Парастаев on 11.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ARFurnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
